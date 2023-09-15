e2bea7264a648ab14abd2e87234200440c21baaf
Giovana O. Silva <giothais.silva@gmail.com>
2023-09-15 18:55:15 -0300

    feat: use outlet in routes

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -11,25 +11,21 @@ import {
 
 export default function Dashboard() {
   return (
-    <Flex flexDir="column" gap="2.5rem" p="2.5rem">
-      <Header />
+    <Tabs>
+      <TabList>
+        <Tab>Análise de avaliações</Tab>
+        <Tab>Informações do sistema</Tab>
+      </TabList>
 
-      <Tabs>
-        <TabList>
-          <Tab>Análise de avaliações</Tab>
-          <Tab>Informações do sistema</Tab>
-        </TabList>
+      <TabPanels>
+        <TabPanel>
+          <p>one!</p>
+        </TabPanel>
 
-        <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Flex>
+        <TabPanel>
+          <p>two!</p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 }
