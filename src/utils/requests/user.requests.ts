1955f01bf5e3bf5a7001a975e8dad7bbe4d1a546
Giovana O. Silva <giothais.silva@gmail.com>
2023-09-19 21:52:39 -0300

    fix: update response type of userRequests.search

--- src/utils/requests/user.requests.ts
+++ src/utils/requests/user.requests.ts
@@ -12,9 +12,11 @@ class UserRequests {
       if (email) query += `&email=${email}`;
       if (role) query += `&role=${role}`;
 
-      const response = await api.get(EndpointsEnum.USER_SEARCH + query);
+      const response = await api.get<UserInterface[]>(
+        EndpointsEnum.USER_SEARCH + query
+      );
 
-      return response.data as UserInterface[];
+      return response.data;
     } catch (error) {
       console.log("ERROR on search by users: ", error);
       return "error";
