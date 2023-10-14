75dd539d70ee8630323804779cf6e33350395744
Giovana O. Silva <giothais.silva@gmail.com>
2023-10-14 20:43:21 -0300

    fix: update pagination method

--- src/utils/requests/comment.request.ts
+++ src/utils/requests/comment.request.ts
@@ -1,20 +1,16 @@
 import api from "../../services/api.service";
 import EndpointsEnum from "../enums/endpoints.enum";
 import CommentPageInterface from "../interfaces/commentPage.interface";
+import MetaInterface from "../interfaces/meta.interface";
 import ReqCommentsFilterInterface from "../interfaces/reqCommentsFilter.interface";
 
 class CommentRequests {
   async getAll(meta: MetaInterface, filter: ReqCommentsFilterInterface) {
     const query = new URLSearchParams({
       order: "asc",
-      take: meta.take.toString(),
+      limit: meta.limit.toString(),
+      page: meta.page.toString(),
     });
-
-    if (meta.cursor) query.set("cursor", meta.cursor);
-    if (meta.hasNextPage !== undefined)
-      query.set("next", `${meta.hasNextPage}`);
-    if (meta.hasPreviousPage !== undefined)
-      query.set("previous", `${meta.hasPreviousPage}`);
 
     if (filter.comment) query.set("comment", filter.comment);
     if (filter.dateDone) query.set("dateDone", filter.dateDone.toISOString());
