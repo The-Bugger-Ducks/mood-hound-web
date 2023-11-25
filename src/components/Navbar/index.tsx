8c1ff137269d58b2ac7e9f1210ce1d824aa83625
Giovana O. Silva <giothais.silva@gmail.com>
2023-11-24 21:30:23 -0300

    feat: create view rule

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,11 +2,13 @@ import logo from "../../assets/images/logo.svg";
 import RoutesEnum from "../../utils/enums/routes.enum";
 import NavitemProps from "./Navitem/props";
 import Navitem from "./Navitem";
+import UserRoleEnum from "../../utils/enums/userRole.enum";
+import userRequests from "../../utils/requests/user.requests";
 
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import { useNavigate } from "react-router";
-import { Card, Image } from "@chakra-ui/react";
+import { Card, Image, useToast } from "@chakra-ui/react";
 import { AiOutlineControl } from "react-icons/ai";
 import { BiUser } from "react-icons/bi";
 import { MdCalendarViewMonth, MdLogout, MdSearch } from "react-icons/md";
@@ -14,9 +16,12 @@ import { MdCalendarViewMonth, MdLogout, MdSearch } from "react-icons/md";
 const Navbar: FC = () => {
   const { signout } = useAuth();
 
+  const toast = useToast();
   const navigate = useNavigate();
 
-  const options: NavitemProps[] = [
+  const [role, setRole] = useState<UserRoleEnum>();
+
+  const [options, setOptions] = useState<NavitemProps[]>([
     {
       id: "01",
       title: "Menu",
@@ -24,12 +29,14 @@ const Navbar: FC = () => {
         {
           id: "0101",
           title: "Visão geral",
+          isVisible: false,
           icon: MdCalendarViewMonth,
           onClick: () => navigate(RoutesEnum.OVERVIEW),
         },
         {
           id: "0102",
           title: "Motor de busca",
+          isVisible: false,
           icon: MdSearch,
           onClick: () => navigate(RoutesEnum.SEARCH_ENGINE),
         },
@@ -43,12 +50,14 @@ const Navbar: FC = () => {
         {
           id: "0201",
           title: "Sistema",
+          isVisible: false,
           icon: AiOutlineControl,
           onClick: () => navigate(RoutesEnum.SYSTEM),
         },
         {
           id: "0202",
           title: "Meu perfil",
+          isVisible: false,
           icon: BiUser,
           onClick: () => navigate(RoutesEnum.USER_UPDATE),
         },
@@ -61,12 +70,112 @@ const Navbar: FC = () => {
         {
           id: "0301",
           title: "Sair",
+          isVisible: true,
           icon: MdLogout,
           onClick: () => signout(),
         },
       ],
     },
-  ];
+  ]);
+
+  useEffect(() => {
+    getUserRole();
+  }, []);
+
+  useEffect(() => {
+    const newOptions = [
+      {
+        id: "01",
+        title: "Menu",
+        subitems: [
+          {
+            id: "0101",
+            title: "Visão geral",
+            isVisible: getVisibilityRule([UserRoleEnum.ADMIN]),
+            icon: MdCalendarViewMonth,
+            onClick: () => navigate(RoutesEnum.OVERVIEW),
+          },
+          {
+            id: "0102",
+            title: "Motor de busca",
+            isVisible: getVisibilityRule([
+              UserRoleEnum.VIEWER,
+              UserRoleEnum.ADMIN,
+            ]),
+            icon: MdSearch,
+            onClick: () => navigate(RoutesEnum.SEARCH_ENGINE),
+          },
+        ],
+      },
+
+      {
+        id: "02",
+        title: "Configurações",
+        subitems: [
+          {
+            id: "0201",
+            title: "Sistema",
+            isVisible: getVisibilityRule([UserRoleEnum.ADMIN]),
+            icon: AiOutlineControl,
+            onClick: () => navigate(RoutesEnum.SYSTEM),
+          },
+          {
+            id: "0202",
+            title: "Meu perfil",
+            isVisible: true,
+            icon: BiUser,
+            onClick: () => navigate(RoutesEnum.USER_UPDATE),
+          },
+        ],
+      },
+
+      {
+        id: "03",
+        subitems: [
+          {
+            id: "0301",
+            title: "Sair",
+            isVisible: true,
+            icon: MdLogout,
+            onClick: () => signout(),
+          },
+        ],
+      },
+    ];
+
+    console.log(newOptions);
+
+    setOptions(newOptions);
+  }, [role]);
+
+  const getUserRole = async () => {
+    const response = await userRequests.getMe();
+
+    if (response === "error") {
+      toast({
+        title: "Não foi possível verificar cargo do usuário.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+
+      return;
+    }
+
+    setRole(response.role);
+  };
+
+  const getVisibilityRule = (permitedRoles: UserRoleEnum[]) => {
+    let isPermited = false;
+
+    permitedRoles.forEach((permitedRole) => {
+      if (permitedRole === role) {
+        isPermited = true;
+      }
+    });
+
+    return isPermited;
+  };
 
   return (
     <Card
@@ -86,12 +195,12 @@ const Navbar: FC = () => {
     >
       <Image src={logo} />
 
-      {options.map((options) => (
+      {options.map((option) => (
         <Navitem
-          key={options.id}
-          id={options.id}
-          subitems={options.subitems}
-          title={options.title}
+          key={option.id}
+          id={option.id}
+          subitems={option.subitems}
+          title={option.title}
         />
       ))}
     </Card>
